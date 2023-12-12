58b1b62da9e287b40a4b4b4a2f33aaddd874da89
Dlanzr <dylan8gt01@gmail.com>
2023-12-12 08:50:51 -0500

    Tara

---
+++ Tarea.py
@@ -0,0 +1,21 @@
+#Laboratorio de Algoritmos y Fundamentos de Programación en Python
+
+#Declaracion de variables
+suma, resta, multiplicacion, divicion = 2 + 4, 3.2 - 1.5, 8 * 5, 6.25/3.2
+#mostrar resultados
+print(f"la suma de 2 + 4 es = {suma}"), print(f"La resta de 3.2 - 1.5 es = {resta}"), print(f"la multiplicacion de 8 * 5 es = {multiplicacion}"), print(f"la divicion de 6.25/3.2 es = {divicion}")
+
+#Contador de letras en el nombre
+
+#Nombre al analizar
+nombre = ("Dylan landazuri alvarado alexander")
+#Mostrar en pantalla la cantidad de letras
+print(len(nombre), "Letras, hay en tu nombre")
+
+#Converciones
+#Conveciones
+flotante_entera = int(3.5)
+
+entera_flotante = float(6)
+#mostrar en pantalla
+print(f"de entera a flotante: {entera_flotante}", f"de flotante a entera: {flotante_entera}")
